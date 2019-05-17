0021861025f207f175694340cd0ef329e99719d9
GabrielSeguel <11512957+TraxidusWolf@users.noreply.github.com>
2019-05-17 05:29:44 -0400

    v0.1.3.10: progress on node mode...

--- delete_non_escential.sh
+++ delete_non_escential.sh
@@ -1,14 +1,16 @@
 #!/bin/bash
 
-find .  ! -name 'delete_non_escential.sh' \
-        ! -name 'script_SMCFPL_run.py' \
-        ! -name 'filter_intracong_log.py' \
+find .  -type d,f \
+	! -name 'delete_non_escential.sh' \
+	! -path "./.git*" \
+	! -path "./TempData_39Bus_v7*" \
+        ! -path "./InputData*" \
+	! -path "./docs*" \
+        ! -name "requirements.txt" \
         ! -name 'LICENSE.md' \
         ! -name 'README.md' \
+        ! -name 'script_SMCFPL_run.py' \
+        ! -name 'filter_intracong_log.py' \
         ! -name 'setup.py' \
-        ! -name 'requirements.txt' \
-        ! -name 'TODO.todo' \
-        ! -path 'InputData*' \
-        ! -path 'docs*' \
-        ! -path 'TempData_39Bus_v7*' \
-        -delete
+        ! -name "TODO.todo" \
+	-delete
